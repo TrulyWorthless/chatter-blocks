8c672c922b6efeddb7e4840df876eae56ddff1de
Amir Shirif <31713018+amshirif@users.noreply.github.com>
2023-03-29 23:27:54 -0500

    Database connection

--- cmd/main.go
+++ cmd/main.go
@@ -7,13 +7,10 @@ import (
 
 func main() {
 	database.ConnectDb()
+
 	app := fiber.New()
 
 	setupRoutes(app)
-
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Div Rhino Trivia App!")
-	})
 
 	app.Listen(":3000")
 }
